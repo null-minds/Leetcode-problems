5afe3cbd1bb0c16011dad7e819da00ba1f33f0af
Anup Kumar Gond <131446137+null-minds@users.noreply.github.com>
2026-08-19 01:51:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.28%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int mySqrt(int n) {
         int ans = 0;
-        for (int i=1; i<=n; i++){
-            long long val = 1LL*i*i;
+        int low = 1, high = n;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            long long val = 1LL * mid * mid;
             if(val <= (long long)n){
-                ans = i;
+                ans = mid;
+                low = mid + 1;
             }
-            else break;
-        } 
+            else high = mid - 1;;
+        }
         return ans;
     }
 };
