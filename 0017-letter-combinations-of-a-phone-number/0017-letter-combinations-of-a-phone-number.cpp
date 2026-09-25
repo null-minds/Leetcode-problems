b2e60bdc610be269acc2e3f67822e0c8c0364dd8
Anup Kumar Gond <131446137+null-minds@users.noreply.github.com>
2026-09-25 18:42:17 +0530

    Time: 0 ms (100%), Space: 9.4 MB (27.46%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,33 @@
 class Solution {
-private:
-    void func(int index, string digits, string combos[],   vector<string> &ans, string s){
+    vector<string> ans;
+    vector<string> combos = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    void func(int index, string &digits, string &s){
         if(index == digits.size()){
             ans.push_back(s);
             return;
         }
 
         // for getting digits digit = digits[i] - '0', it converts that into a number( ASCII - ASCII = number)
-        int num = digits[index] - '0';
-        for(int i=0; i<combos[num].size(); i++){
-            func(index+1, digits, combos, ans, s + combos[num][i]);
+        // int num = digits[index] - '0';
+        // for(int i=0; i<combos[num].size(); i++){
+        //     func(index+1, digits, combos, ans, s + combos[num][i]);
+        // }
+
+        string letters = combos[digits[index] - '0'];
+
+        for (char ch : letters) {
+            s.push_back(ch);
+            func(index + 1, digits, s);
+            s.pop_back();
         }
     }
 
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string> ans;
         string s="";
-        string combos[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-        func(0, digits, combos, ans, s);
+        func(0, digits, s);
         return ans;
     }
 };
